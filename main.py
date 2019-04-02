1d0ded2a91d8f39bb27e7ea278afe112836ae1bb
magg666 <mw23127@gmail.com>
2019-04-03 00:23:20 +0200

    added functions for main page

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from flask import Flask, render_template, url_for
-from data import queries
+from flask import Flask, render_template, request, jsonify
+from data_manager import shows as shows
 
 app = Flask('codecool_series')
 
 
 @app.route('/')
 def index():
-    shows = queries.get_shows()
-    return render_template('index.html', shows=shows)
+    # all_shows = shows.get_ordered(number=0)
+    return render_template('index.html')
+
+
+@app.route('/page', methods=['POST'])
+def shows_pages():
+    json_number = request.get_json()
+    number = json_number['number']
+    all_shows = shows.get_ordered(number)
+    counted_shows = shows.count_all()
+    return jsonify({'shows': all_shows,
+                    'counted_shows': counted_shows})
 
 
 @app.route('/design')
