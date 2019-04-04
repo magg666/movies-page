c0b074e8da5e765eeb6e26a355bf35608672927e
magg666 <mw23127@gmail.com>
2019-04-04 17:06:52 +0200

    added formatted data for shows added homepage and trailer for data

--- static/script/data_formatter.js
+++ static/script/data_formatter.js
@@ -9,8 +9,10 @@ function formatShowsData(data) {
                 year = show['year'],
                 runtime = show['runtime'] + ' min.',
                 genres = show['genre_name'] !== null ? show['genre_name']: 'Suggest genre!'.fontcolor('red'),
-                rating = Number.parseFloat(show['rating']).toFixed(2);
-            return [id, title, year, runtime, genres, rating]
+                rating = Number.parseFloat(show['rating']).toFixed(2),
+                homepage = show['homepage'] !== null ? `<a href="${show['homepage']}" target="_blank">${title} homepage</a>` : 'unknown',
+                trailer = show['trailer'] !== null ? `<a href="${show['trailer']}" target="_blank">${title} trailer</a>` : 'unknown';
+            return [id, title, year, runtime, genres, rating, homepage, trailer]
         })
     };
 }
