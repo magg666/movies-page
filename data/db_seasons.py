a44c752625a39953481c920e83ee368eee2ccfb4
magg666 <mw23127@gmail.com>
2019-04-07 10:03:26 +0200

    added queries for seasons getting title by season id

--- data/db_seasons.py
+++ data/db_seasons.py
@@ -13,5 +13,16 @@ def get_seasons_for_show(cursor, show_id):
     cursor.execute(sql_str, {'show_id': show_id})
     return cursor.fetchall()
 
+
+@con.connection_handler
+def get_season_title_by_id(cursor, season_id):
+    sql_str = """
+    SELECT title FROM seasons
+    WHERE id = %(season_id)s
+    """
+    cursor.execute(sql_str, {'season_id': season_id})
+    season_title = cursor.fetchone()
+    return season_title
+
 # UPDATE
 # DELETE
