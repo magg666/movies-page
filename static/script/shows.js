37d1e5155b931323be323928bd01badb6dbb6a82
magg666 <mw23127@gmail.com>
2019-04-07 10:09:13 +0200

    added comments

--- static/script/shows.js
+++ static/script/shows.js
@@ -1,6 +1,6 @@
 import {postData, getData} from "./communication.js";
 import {formatShowsData} from "./data_formatter.js";
-import {clearElement} from "./dom.js";
+import {clearElement, openModal} from "./dom.js";
 
 export {displayShowsPage}
 
@@ -20,10 +20,70 @@ function displayShowsTable(way=0) {
         'sort_by': showsParameters.sortBy,
         'order': showsParameters.sortOrder
     };
-    postData('/page', pageNumber, createShowsTable);
+    postData('/page', pageNumber, checkShowsData);
     return showsParameters.offsetForShows
 }
 
+// ----- validate data from server and inform user if there is error ----
+function checkShowsData(data) {
+    if(data['shows'] === 'error'){
+        openModal('user', 'Page is under construction.Try again later' )
+    }
+    else {
+        createShowsTable(data)
+    }
+}
+
+// ------ create table with shows------------
+function createShowsTable(data) {
+    let showsTableBody = document.getElementById('shows-table-body');
+    let formattedShows = formatShowsData(data);
+    let showIdIndex = 0;
+
+    showsParameters.allShows = formattedShows.countedShows;
+    checkDisplayingPaginationButtons();
+
+    clearElement(showsTableBody);
+
+    for (let show of formattedShows['allShows']) {
+        let allCells = '';
+        let cell = '';
+        for (let i = 1; i < show.length; i++) {
+            if (i === 1) {
+                cell = `<td class="shows-cell${i}"><a href="/show/${show[showIdIndex]}">${show[i]}</a></td>`;
+            } else {
+                cell = `<td class="shows-cell${i}">${show[i]}</td>`
+            }
+            allCells += cell
+        }
+        let row = `<tr>${allCells}</tr>`;
+
+        showsTableBody.insertAdjacentHTML('beforeend', row)
+    }
+}
+
+//-------- define sorting parameters-----
+function addSorting(){
+    let sortButtons = document.querySelectorAll('.sort');
+    for(let button of sortButtons){
+        button.addEventListener('click', function () {
+            showsParameters.offsetForShows = 0;
+            showsParameters.sortBy = button.parentElement.dataset.sortBy;
+            defineSortingOrder(button);
+            displayShowsTable()
+        })
+    }
+}
+
+function defineSortingOrder(button) {
+    if(button.innerHTML.trim() === '<i class="fas fa-sort-down"></i>'){
+        showsParameters.sortOrder = 'DESC'
+    } else if(button.innerHTML.trim() === '<i class="fas fa-sort-up"></i>'){
+        showsParameters.sortOrder = 'ASC'
+    } return showsParameters.sortOrder
+}
+
+// --------- pagination --------
 function addPagination() {
     let nextButton = document.getElementById('shows-pagination-next');
     let previousButton = document.getElementById('shows-pagination-previous');
@@ -58,55 +118,7 @@ function checkDisplayingPaginationButtons() {
     }
 }
 
-function createShowsTable(data) {
-    let showsTableBody = document.getElementById('shows-table-body');
-    let formattedShows = formatShowsData(data);
-    let showIdIndex = 0;
-
-    showsParameters.allShows = formattedShows.countedShows;
-    checkDisplayingPaginationButtons();
-
-    clearElement(showsTableBody);
-
-    for (let show of formattedShows['allShows']) {
-        let allCells = '';
-        let cell = '';
-        for (let i = 1; i < show.length; i++) {
-            if (i === 1) {
-                cell = `<td class="shows-cell${i}"><a href="/show/${show[showIdIndex]}">${show[i]}</a></td>`;
-            } else {
-                cell = `<td class="shows-cell${i}">${show[i]}</td>`
-            }
-            allCells += cell
-        }
-        let row = `<tr>${allCells}</tr>`;
-
-        showsTableBody.insertAdjacentHTML('beforeend', row)
-    }
-}
-
-//-------- sorting-----
-
-
-function addSorting(){
-    let sortButtons = document.querySelectorAll('.sort');
-    for(let button of sortButtons){
-        button.addEventListener('click', function () {
-            showsParameters.offsetForShows = 0;
-            showsParameters.sortBy = button.parentElement.dataset.sortBy;
-            defineSortingOrder(button);
-            displayShowsTable()
-        })
-    }
-}
-
-function defineSortingOrder(button) {
-    if(button.innerHTML.trim() === '<i class="fas fa-sort-down"></i>'){
-        showsParameters.sortOrder = 'DESC'
-    } else if(button.innerHTML.trim() === '<i class="fas fa-sort-up"></i>'){
-        showsParameters.sortOrder = 'ASC'
-    } return showsParameters.sortOrder
-}
+//--------- all functions for main page
 
 function displayShowsPage() {
     displayShowsTable();
