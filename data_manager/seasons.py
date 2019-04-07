a0519a087a4517b131b2a7111e0660e9d502eb14
magg666 <mw23127@gmail.com>
2019-04-07 10:05:50 +0200

    added error handling added getting title

--- data_manager/seasons.py
+++ data_manager/seasons.py
@@ -1,12 +1,28 @@
 from data import db_seasons as seasons_manager
+from log import logging_rules as log
 
 
 def get_for_show(show_id):
-    seasons = seasons_manager.get_seasons_for_show(show_id)
-    for season in seasons:
-        if season['overview'] is None:
-            if season['title'] == 'Specials':
-                season['overview'] = 'Trailers, one-time movies, behind scenes and many more...'
-            else:
-                season['overview'] = 'This season do not has a overview yet. Join us and write your own summary!'
-    return seasons
+    try:
+        seasons = seasons_manager.get_seasons_for_show(show_id)
+        for season in seasons:
+            if season['overview'] is None:
+                if season['title'] == 'Specials':
+                    season['overview'] = 'Trailers, one-time movies, behind scenes and many more...'
+                else:
+                    season['overview'] = 'This season do not has a overview yet. Join us and write your own summary!'
+        return seasons
+    except Exception as err:
+        log.logger.error('%s', err)
+        log.logging.exception(err)
+        return []
+
+
+def get_title(season_id):
+    try:
+        season_title = seasons_manager.get_season_title_by_id(season_id)
+        return season_title['title']
+    except Exception as err:
+        log.logger.error('%s', err)
+        log.logging.exception(err)
+        return ""
