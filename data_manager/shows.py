b475ff811613effab574331a722afe9992b2a4d9
magg666 <mw23127@gmail.com>
2019-04-07 10:06:04 +0200

    added error handling added getting title

--- data_manager/shows.py
+++ data_manager/shows.py
@@ -1,4 +1,15 @@
+from log import logging_rules as log
 from data import db_shows as shows_manager
+
+
+class WrongSort(Exception):
+    """ If there is problem with sort functions"""
+    pass
+
+
+class ReadingProblem(Exception):
+    """ If there is problem reading data"""
+    pass
 
 
 def count_all():
@@ -7,22 +18,42 @@ def count_all():
 
 
 def get_ordered(json_data):
-    number = json_data['number']
-    sort_by = json_data['sort_by']
-    order = json_data['order']
+    try:
+        number = json_data['number']
+        sort_by = json_data['sort_by']
+        order = json_data['order']
+        shows_data = shows_manager.get_shows(number, sort_by, order)
+        for data in shows_data:
+            if data['genre_name'] is not None:
+                data['genre_name'] = ", ".join((data['genre_name'].split(' '))[0:3])
 
-    shows_data = shows_manager.get_shows(number, sort_by, order)
-    for data in shows_data:
-        if data['genre_name'] is not None:
-            data['genre_name'] = ", ".join((data['genre_name'].split(' '))[0:3])
+        return shows_data
 
-    return shows_data
+    except Exception as err:
+        log.logger.error('%s', err)
+        log.logging.exception(err)
+        raise WrongSort
 
 
 def get_one(show_id):
-    one_show = shows_manager.get_one_show(show_id)
+    try:
+        one_show = shows_manager.get_one_show(show_id)
+        if one_show['trailer']:
+            one_show['trailer'] = one_show['trailer'].replace('watch?v=', 'embed/')
+        if one_show['year']:
+            one_show['year'] = int(one_show['year'])
+        return one_show
+    except Exception as err:
+        log.logger.error('%s', err)
+        log.logging.exception(err)
+        return {}
 
-    if one_show['trailer']:
-        one_show['trailer'] = one_show['trailer'].replace('watch?v=', 'embed/')
-    return one_show
 
+def get_title_by_season_id(season_id):
+    try:
+        show_title = shows_manager.get_show_title_by_season(season_id)
+        return show_title['title']
+    except Exception as err:
+        log.logger.error('%s', err)
+        log.logging.exception(err)
+        return ""
