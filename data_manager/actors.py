eb246a195f5efed0cf473c06d67b97c672fd2d84
magg666 <mw23127@gmail.com>
2019-04-07 10:05:30 +0200

    added error handling added getting one actor

--- data_manager/actors.py
+++ data_manager/actors.py
@@ -1,8 +1,39 @@
 from data import db_actors as actors_manager
+from log import logging_rules as log
+
+
+class ReadingProblem(Exception):
+    """ If there is problem reading data"""
+    pass
 
 
 def get_for_show(show_id):
-    actors = actors_manager.get_all_actors_for_one_show(show_id)
-    return actors
+    try:
+        return actors_manager.get_all_actors_for_one_show(show_id)
+    except Exception as err:
+        log.logger.error('%s', err)
+        log.logging.exception(err)
+        return []
 
+
+def get_all_with_shows():
+    try:
+        return actors_manager.get_all_actors_with_shows()
+
+    except Exception as err:
+        log.logger.error('%s', err)
+        log.logging.exception(err)
+        raise ReadingProblem
+
+
+def get_one(actor_id):
+    try:
+        one_actor = actors_manager.get_one_actor(actor_id)
+        if one_actor['death'] is None:
+            one_actor['death'] = 'still alive'
+            return one_actor
+    except Exception as err:
+        log.logger.error('%s', err)
+        log.logging.exception(err)
+        raise ReadingProblem
 
