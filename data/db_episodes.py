edd1afcdcf59e178dbd6b7b4cd11fdc99e235ff3
magg666 <mw23127@gmail.com>
2019-04-07 10:12:29 +0200

    added queries for episodes

---
+++ data/db_episodes.py
@@ -0,0 +1,19 @@
+from db_connection import db_connect as con
+
+
+# CREATE
+# READ
+@con.connection_handler
+def get_episodes_for_season(cursor, season_id):
+    sql_str = """
+    SELECT id, title, episode_number, overview FROM episodes
+    WHERE season_id = %(season_id)s
+    """
+    cursor.execute(sql_str, {'season_id': season_id})
+    episodes_data = cursor.fetchall()
+    return episodes_data
+
+
+
+# UPDATE
+# DELETE
