f98c2ca4a114bd260084cc47e618bb685aae87b4
magg666 <mw23127@gmail.com>
2019-04-07 10:08:42 +0200

    added window on error handler added functions for specific pages

--- static/script/main.js
+++ static/script/main.js
@@ -1,6 +1,29 @@
 import {displayShowsPage} from "./shows.js";
+import {addEventListenerToEpisodeButton} from "./episodes.js";
+import {sendErrorLogsToServer} from "./communication.js";
+import {newDataActors} from "./actors.js";
 
 function main() {
-    displayShowsPage()
+
+    if (window.location.pathname === "/") {
+        displayShowsPage();
+    } else if (window.location.pathname.startsWith("/show/")) {
+        addEventListenerToEpisodeButton();
+    } else if (window.location.pathname.startsWith("/actors")) {
+        newDataActors()
+    }
+
 }
+
+// error handler
+window.addEventListener('error', function (e) {
+    let stack = e.error.stack;
+    let message = e.error.toString();
+    if (stack) {
+        message += '\n' + stack;
+    }
+    sendErrorLogsToServer(message)
+});
+
+
 main();
