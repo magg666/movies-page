3c894f727c4fe27801e1d589970fbcebecc40192
magg666 <mw23127@gmail.com>
2019-04-07 10:02:54 +0200

    added queries for actors getting data from db

--- data/db_actors.py
+++ data/db_actors.py
@@ -17,5 +17,33 @@ def get_all_actors_for_one_show(cursor, show_id):
     return actors
 
 
+@con.connection_handler
+def get_all_actors_with_shows(cursor):
+    sql_str = """
+    SELECT a.name, a.id, json_object_agg(DISTINCT s.id, s.title) as shows_object FROM actors a
+    JOIN show_characters sc on a.id = sc.actor_id
+    JOIN shows s on sc.show_id = s.id
+    GROUP BY a.name, a.id
+    """
+
+    cursor.execute(sql_str)
+    actors_with_shows = cursor.fetchall()
+    return actors_with_shows
+
+
+@con.connection_handler
+def get_one_actor(cursor, actor_id):
+    sql_str = """
+    SELECT a.name, a.birthday, a.death, a.biography, json_object_agg(sc.character_name, s.title) as show_role
+    FROM actors a
+       JOIN show_characters sc on a.id = sc.actor_id
+       JOIN shows s on sc.show_id = s.id
+    WHERE a.id = %(actor_id)s
+    GROUP BY a.name, a.birthday, a.death, a.biography
+    """
+    cursor.execute(sql_str, {'actor_id': actor_id})
+    one_actor = cursor.fetchone()
+    return one_actor
+
 # UPDATE
 # DELETE
