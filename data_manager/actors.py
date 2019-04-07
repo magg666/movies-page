3920e2c7843ca25794db8123580a3609c931e9ae
magg666 <mw23127@gmail.com>
2019-04-07 22:12:37 +0200

    added validating actors data

--- data_manager/actors.py
+++ data_manager/actors.py
@@ -1,9 +1,16 @@
+from datetime import datetime
+
 from data import db_actors as actors_manager
 from log import logging_rules as log
 
 
 class ReadingProblem(Exception):
-    """ If there is problem reading data"""
+    """ If there is problem with reading data"""
+    pass
+
+
+class SavingProblem(Exception):
+    """ If there is problem with saving data"""
     pass
 
 
@@ -37,3 +44,70 @@ def get_one(actor_id):
         log.logging.exception(err)
         raise ReadingProblem
 
+
+def is_data_keys_valid(actor_data):
+    if 'actor_name' in actor_data and \
+            'actor_birthday' in actor_data and \
+            'actor_death' in actor_data and \
+            'actor_biography' in actor_data:
+        return True
+    else:
+        return False
+
+
+def is_existing(actor_data):
+    try:
+        return actors_manager.exists_already(actor_data['actor_name'])
+    except Exception as err:
+        log.logger.error('%s', err)
+        log.logging.exception(err)
+        raise ReadingProblem
+
+
+def check_actors_name(actor_data):
+    if actor_data['actor_name'].strip() != '':
+        return True
+
+
+def check_actors_birth(actor_data):
+    try:
+        if datetime.strptime(actor_data['actor_birthday'], '%Y-%m-%d'):
+            return True
+    except ValueError:
+        return False
+
+
+def check_actors_death(actor_data):
+    try:
+        if actor_data['actor_death'] == '' or datetime.strptime(actor_data['actor_death'], '%Y-%m-%d'):
+            return True
+    except ValueError:
+        return False
+
+
+def check_actors_biography(actor_data):
+    return len(actor_data['actor_biography']) > 20
+
+
+def is_data_valid(actor_data):
+    if is_data_keys_valid(actor_data) and \
+            check_actors_name(actor_data) and \
+            check_actors_birth(actor_data) and \
+            check_actors_death(actor_data) and \
+            check_actors_biography(actor_data):
+        return True
+
+
+def add_new(actor_data):
+    try:
+        if actor_data['actor_death'] == '':
+            actor_data['actor_death'] = None
+
+        actors_manager.add_new_actor(actor_data)
+
+    except Exception as err:
+        log.logger.error(
+            '{0}, Data to be added: {actor_name}/{actor_birthday}/{actor_death}/{actor_biography}'.format(err,
+                                                                                                          **actor_data))
+        log.logging.exception(err)
+        raise SavingProblem
