8b970e4932c3a053020741742cb31222f4e9ede9
magg666 <mw23127@gmail.com>
2019-04-08 09:55:30 +0200

    added route for shows by genres added imports

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, jsonify
+
 from data import queries
+from data_manager import genres as genre
 
 app = Flask('codecool_series')
 
@@ -8,6 +10,24 @@ app = Flask('codecool_series')
 def index():
     shows = queries.get_shows()
     return render_template('index.html', shows=shows)
+
+
+@app.route('/choose-genre', methods=['GET'])
+def show_genres():
+    genres = genre.get_all()
+    return render_template('select_genre.html',
+                           genres=genres)
+
+
+@app.route('/choose-genre', methods=['POST'])
+def search_by_genre():
+    genre_id = request.get_json()
+    try:
+        shows_by_genre = genre.get_shows_by(genre_id)
+        return jsonify({'state': shows_by_genre})
+    except genre.ReadingProblem:
+        return jsonify({'state': 'error'})
+
 
 
 @app.route('/design')
